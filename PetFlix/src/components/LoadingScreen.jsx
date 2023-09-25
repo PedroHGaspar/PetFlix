907f05d3b96c1fde4cf0560d4687cb558859261f
PedroHGaspar <ordep12@hotmail.com.br>
2023-09-25 20:40:40 -0300

    Feature: new home screen.

--- PetFlix/src/components/LoadingScreen.jsx
+++ PetFlix/src/components/LoadingScreen.jsx
@@ -1,25 +1,49 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 function LoadingScreen() {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    setTimeout(() => {
-      navigate("/home");
-    }, 3000);
-  }, [navigate]);
+  const handleHomeClick = () => {
+    navigate("/home");
+  };
+
+  const handleHomeCachorrosClick = () => {
+    navigate("/homecachorros");
+  };
 
   return (
-    <div className="loading-screen">
-      <img
-        className="img-logo-loading-screen"
-        src="/petflix-logo.png"
-        alt="Petflix Logo"
-      />
+    <div className="align-home-text-images">
+      <div>
+        <span>Quem está assistindo?</span>
+      </div>
+      <div className="loading-content">
+        <div
+          className="loading-button-cachorro cachorro-perfil"
+          style={{ backgroundImage: `url('/cachorro.jpg')` }}
+          onClick={handleHomeClick}
+        >
+
+        </div>
+        <div
+          className="loading-button-gato gato-perfil"
+          style={{ backgroundImage: `url('/gato.jpg')` }}
+          onClick={handleHomeCachorrosClick}
+        >
+
+        </div>
+      </div>
     </div>
   );
 }
 
 export default LoadingScreen;
+
+
+
+// <img
+// className="img-logo-loading-screen"
+// src="/petflix-logo.png"
+// alt="Petflix Logo"
+// />
