4645c4c67693599ab209ccbbbfcf73b5afc7c387
PedroHGaspar <ordep12@hotmail.com.br>
2023-09-27 00:00:24 -0300

    Feature: React Modal implemented with IFrame for video reproduction

--- PetFlix/src/components/MovieListDogs.jsx
+++ PetFlix/src/components/MovieListDogs.jsx
@@ -2,27 +2,27 @@ import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Modal from 'react-modal';
 import './CustomArrows.css';
 import { CustomNextArrow, CustomPrevArrow } from './CustomArrows';
 import './MovieList.css';
 
 const filmes = [
-    { titulo: 'Gato 1', imgSource: '/bolt.jpeg', descricao: '.' },
-    { titulo: 'Gato 2', imgSource: '/marley.jpeg', descricao: '.' },
-    { titulo: 'Gato 3', imgSource: '/bolt.jpeg', descricao: '.' },
-    { titulo: 'Gato 4', imgSource: '/marley.jpeg', descricao: '.' },
-    { titulo: 'Gato 5', imgSource: '/bolt.jpeg', descricao: '.' },
-    { titulo: 'Gato 6', imgSource: '/marley.jpeg', descricao: '.' },
+    { titulo: 'Gato 1', imgSource: '/bolt.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 2', imgSource: '/marley.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 1', imgSource: '/bolt.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 2', imgSource: '/marley.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 1', imgSource: '/bolt.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 2', imgSource: '/marley.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
 ];
 
 const filmesDormir = [
-    { titulo: 'Gato 1', imgSource: '/bolt.jpeg', descricao: '.' },
-    { titulo: 'Gato 2', imgSource: '/marley.jpeg', descricao: '.' },
-    { titulo: 'Gato 3', imgSource: '/bolt.jpeg', descricao: '.' },
-    { titulo: 'Gato 4', imgSource: '/marley.jpeg', descricao: '.' },
-    { titulo: 'Gato 5', imgSource: '/bolt.jpeg', descricao: '.' },
-    { titulo: 'Gato 6', imgSource: '/marley.jpeg', descricao: '.' },
-    // Adicione mais filmes para gatos dormirem aqui
+    { titulo: 'Gato 1', imgSource: '/bolt.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 2', imgSource: '/marley.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 1', imgSource: '/bolt.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 2', imgSource: '/marley.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 1', imgSource: '/bolt.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
+    { titulo: 'Gato 2', imgSource: '/marley.jpeg', descricao: '.', videoUrl: 'https://www.youtube.com/embed/5RXA4F1wJYc?si=HiShhoAxEEQdNEFX' },
 ];
 
 const MovieListDogs = () => {
@@ -31,11 +31,16 @@ const MovieListDogs = () => {
 
     const [isHoveredDormir, setIsHoveredDormir] = useState(null);
     const [showTextDormir, setShowTextDormir] = useState(Array(filmesDormir.length).fill(false));
-    //aqui é criado um estado local para controlar a visibilidade de cada filme INDEPENDENTE.
-    // Array(filmes.length) cria um novo array com um número de elementos igual ao comprimento (número de filmes) do array filmes(objeto)
-    // o método fill(false) preenche esse novo array com valores false. inicialmente para cada filme o estado showText é definido como false, o que significa que o texto não está visível.
-    //depois disso é só manipular o showText e o setShowText para mostrar o texto.
 
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const openVideoModal = (videoUrl) => {
+        setSelectedVideo(videoUrl);
+    };
+
+    const closeVideoModal = () => {
+        setSelectedVideo(null);
+    };
 
     const settings = {
         infinite: true,
@@ -47,9 +52,9 @@ const MovieListDogs = () => {
     };
 
     const toggleTextVisibility = (index) => {
-        const updatedShowText = [...showText];//aqui usando o spread eu clonei o array showText
-        updatedShowText[index] = !updatedShowText[index];//aqui eu pego um valor especifico do array e inverte ele com o operador !
-        setShowText(updatedShowText);//aqui eu atualizo o estado showText com o novo array, informando que o estado foi alterado e o texto será mostrado do item daquele índice em que o mouse está pegando.
+        const updatedShowText = [...showText];
+        updatedShowText[index] = !updatedShowText[index];
+        setShowText(updatedShowText);
     };
 
     const toggleTextVisibilityDormir = (index) => {
@@ -64,12 +69,12 @@ const MovieListDogs = () => {
                 <p className='text-title-list'>Para o seu cachorro assistir</p>
             </div>
 
-            <Slider {...settings}> {/* aqui no spread é renderizado o Slider(componente pronto do react), isso cria o carrossel */}
-                {filmes.map((filme, index) => (//aqui é criado um map padrão para crIAr um conjunto de divs, uma div pra cada filme(pra isso q serve o map)
+            <Slider {...settings}>
+                {filmes.map((filme, index) => (
                     <div
-                        key={index}//aqui faz com que cada filme tenha seu proprio index, seu proprio indicador unico para nao haver falhas de renderizaçao e pra cada filme ser único
+                        key={index}
+                        onClick={() => openVideoModal(filme.videoUrl)}
                         className={`movie-slide ${showText[index] ? 'show-text' : ''} ${isHovered === index ? 'hovered' : ''}`}
-                         //aqui fiz com que a classe seja dinamica de acordo com o estado do componente.
                         onMouseEnter={() => {
                             toggleTextVisibility(index);
                             setIsHovered(index);
@@ -97,6 +102,7 @@ const MovieListDogs = () => {
                 {filmesDormir.map((filme, index) => (
                     <div
                         key={index}
+                        onClick={() => openVideoModal(filme.videoUrl)}
                         className={`movie-slide ${showTextDormir[index] ? 'show-text' : ''} ${isHoveredDormir === index ? 'hovered' : ''}`}
                         onMouseEnter={() => {
                             toggleTextVisibilityDormir(index);
@@ -116,6 +122,22 @@ const MovieListDogs = () => {
                     </div>
                 ))}
             </Slider>
+
+            <Modal
+                isOpen={selectedVideo !== null}
+                onRequestClose={closeVideoModal}
+                contentLabel="Reprodutor de Vídeo"
+                ariaHideApp={false}
+            >
+                {selectedVideo && (
+                    <iframe
+                        width="1260"
+                        height="762"
+                        src={selectedVideo}
+                        title="Reprodutor de Vídeo"
+                    ></iframe>
+                )}
+            </Modal>
         </div>
     );
 };
