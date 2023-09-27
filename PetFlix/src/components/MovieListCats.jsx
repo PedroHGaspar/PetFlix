567c0aa9eb99d08874de1d796a793c6e8df7c0e3
PedroHGaspar <ordep12@hotmail.com.br>
2023-09-27 00:41:50 -0300

    Fix: comments restored

--- PetFlix/src/components/MovieListCats.jsx
+++ PetFlix/src/components/MovieListCats.jsx
@@ -31,6 +31,10 @@ const MovieListCats = () => {
 
     const [isHoveredDormir, setIsHoveredDormir] = useState(null);
     const [showTextDormir, setShowTextDormir] = useState(Array(filmesDormir.length).fill(false));
+    //aqui é criado um estado local para controlar a visibilidade de cada filme INDEPENDENTE.
+    // Array(filmes.length) cria um novo array com um número de elementos igual ao comprimento (número de filmes) do array filmes(objeto)
+    // o método fill(false) preenche esse novo array com valores false. inicialmente para cada filme o estado showText é definido como false, o que significa que o texto não está visível.
+    //depois disso é só manipular o showText e o setShowText para mostrar o texto.
 
     const [selectedVideo, setSelectedVideo] = useState(null);
 
@@ -52,9 +56,9 @@ const MovieListCats = () => {
     };
 
     const toggleTextVisibility = (index) => {
-        const updatedShowText = [...showText];
-        updatedShowText[index] = !updatedShowText[index];
-        setShowText(updatedShowText);
+        const updatedShowText = [...showText];//aqui usando o spread eu clonei o array showText
+        updatedShowText[index] = !updatedShowText[index];//aqui eu pego um valor especifico do array e inverte ele com o operador !
+        setShowText(updatedShowText);//aqui eu atualizo o estado showText com o novo array, informando que o estado foi alterado e o texto será mostrado do item daquele índice em que o mouse está pegando.
     };
 
     const toggleTextVisibilityDormir = (index) => {
@@ -69,12 +73,13 @@ const MovieListCats = () => {
                 <p className='text-title-list'>Para o seu gato assistir</p>
             </div>
 
-            <Slider {...settings}>
-                {filmes.map((filme, index) => (
+            <Slider {...settings}>{/* aqui no spread é renderizado o Slider(componente pronto do react), isso cria o carrossel */}
+                {filmes.map((filme, index) => (//aqui é criado um map padrão para crIAr um conjunto de divs, uma div pra cada filme(pra isso q serve o map)
                     <div
-                        key={index}
+                        key={index}//aqui faz com que cada filme tenha seu proprio index, seu proprio indicador unico para nao haver falhas de renderizaçao e pra cada filme ser único
                         onClick={() => openVideoModal(filme.videoUrl)}
                         className={`movie-slide ${showText[index] ? 'show-text' : ''} ${isHovered === index ? 'hovered' : ''}`}
+                         //aqui fiz com que a classe seja dinamica de acordo com o estado do componente.
                         onMouseEnter={() => {
                             toggleTextVisibility(index);
                             setIsHovered(index);
